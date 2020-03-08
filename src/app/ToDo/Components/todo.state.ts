16ea8068bcae28d0cea2f8b0565c8050017ffdef
amir-saeed <devloggeruk@gmail.com>
2020-03-08 01:22:15 +0000

    Added component todo

---
+++ src/app/ToDo/Components/todo.state.ts
@@ -0,0 +1,13 @@
+import ToDo from './todo.model';
+
+export default class ToDoState {
+  ToDos: Array<ToDo>;
+  ToDoError: Error;
+}
+
+export const initializeState = (): ToDoState => {
+  return {
+    ToDos: Array<ToDo>(),
+    ToDoError: null
+  };
+};
